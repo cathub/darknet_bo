799e5798a5653dcf0cf6362183730f71bc9895f4
cathub <boli.ufl@gmail.com>
2020-09-29 00:58:23 -0700

    draw line between current and previous.

--- src/image.c
+++ src/image.c
@@ -171,6 +171,34 @@ void draw_label(image a, int r, int c, image label,
   }
 }
 
+void draw_line(image a, int x1, int y1, int x2, int y2, float r, float g, float b) {
+  int i;
+  if (x1 < 0)
+    x1 = 0;
+  if (x1 >= a.w)
+    x1 = a.w - 1;
+  if (x2 < 0)
+    x2 = 0;
+  if (x2 >= a.w)
+    x2 = a.w - 1;
+
+  if (y1 < 0)
+    y1 = 0;
+  if (y1 >= a.h)
+    y1 = a.h - 1;
+  if (y2 < 0)
+    y2 = 0;
+  if (y2 >= a.h)
+    y2 = a.h - 1;
+  
+  for (i = x1; i <= x2; ++i) {
+    int j = y1 + (i -x1) * (y2- y1)/(x2-x1);
+    a.data[i + j * a.w + 0 * a.w * a.h] = r;
+    a.data[i + j * a.w + 1 * a.w * a.h] = g;
+    a.data[i + j * a.w + 2 * a.w * a.h] = b;
+  }
+}
+
 void draw_box(image a, int x1, int y1, int x2, int y2, float r, float g, float b) {
   int i;
   if (x1 < 0)
@@ -715,6 +743,9 @@ void draw_detections(image im, detection * dets, int num, float thresh,
       printf("Can not find an empty slot!\n");
       continue;
     }
+
+    draw_line(im, person_cen_prev[assigned_prev_index][0], person_cen_prev[assigned_prev_index][1],
+              person_cen_cur[i][0], person_cen_cur[i][1], 255, 255, 255);
 
     // Add speed for person.
     char speed_object[64];
